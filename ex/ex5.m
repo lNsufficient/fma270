e8ff39ffe48946edf03c345e4175c9d361634df6
lNsufficient <edvard.jo@gmail.com>
2017-03-15 16:42:15 +0100

    ex5 halvt klar

--- ex/ex5.m
+++ ex/ex5.m
@@ -1,4 +1,5 @@
 clear;
+%load('ex5_calculations')
 
 im{1} = imread('mods/ex5a.jpg');
 im{2} = imread('mods/ex5b.jpg');
@@ -22,75 +23,143 @@ end
 i_prime = 2; %This figure seemed to some parts in common with all of the images
 
 %% Trying to merge to images
-ind_prime = 2; %these seem the most similar, for testing I will start with them
-ind = 4
-common_ind = ind_prime*10+ind;
+for ind_prime = 1:4
+    for ind = 1:4
+        if ind == ind_prime
+            continue;
+        end
+        common_ind = ind_prime*10+ind;
 
-matches{common_ind} = vl_ubcmatch(d{ind_prime}, d{ind})
-%matches_tmp = vl_ubcmatch(d{ind_prime}, d{ind})
-xA = f{ind_prime}(1:2, matches{common_ind}(1,:));
-xB = f{ind}(1:2, matches{common_ind}(2,:));
+        matches{common_ind} = vl_ubcmatch(d{ind_prime}, d{ind});
+        %matches_tmp = vl_ubcmatch(d{ind_prime}, d{ind})
+        xA = f{ind_prime}(1:2, matches{common_ind}(1,:));
+        xB = f{ind_prime}(1:2, matches{common_ind}(1,:));
+        xA = f{ind}(1:2, matches{common_ind}(2,:));
 
-matching_features = length(matches{common_ind});
+        matching_features = length(matches{common_ind});
 
-%Pflat image points
-xa = [xA; ones(1,matching_features)];
-xb = [xB; ones(1,matching_features)];
+        %Pflat image points
+        xa = [xA; ones(1,matching_features)];
+        xb = [xB; ones(1,matching_features)];
 
-%% Ransac to find their homography
+        %% Ransac to find their homography
 
-nbrSets = 100;
-maxDist = 5;
-bestInliers = 0;
-N = matching_features;
-k = 4; %As few as possible to find the homography
+        nbrSets = 100;
+        maxDist = 5;
+        bestInliers = 0;
+        N = matching_features;
+        k = 4; %As few as possible to find the homography
 
-for i = 1:nbrSets
-    randind = randperm(N,k);
-    
-    H = getH(xa(:,randind), xb(:,randind));
-    
-    Hxa = pflat(H*xa);
-    dists = sqrt(sum((xb - Hxa).^2, 1));
-    inliers = dists <= maxDist;
-    
-    if sum(inliers) > sum(bestInliers)
-        bestInliers = inliers;
-        bestIndices = randind;
+        for i = 1:nbrSets
+            randind = randperm(N,k);
+
+            H = getH(xa(:,randind), xb(:,randind));
+
+            Hxa = pflat(H*xa);
+            dists = sqrt(sum((xb - Hxa).^2, 1));
+            inliers = dists <= maxDist;
+
+            if sum(inliers) > sum(bestInliers)
+                bestInliers = inliers;
+                bestIndices = randind;
+            end
+        end
+
+        nbr_inliers = sum(bestInliers)
+        %bestH = getH(xa(:,bestIndices), xb(:,bestIndices));
+        %should be done using normalization when it works
+        Na_bestInd = getN(xa(:,bestIndices));
+        Nb_bestInd = getN(xb(:,bestIndices));
+        bestH = getH(xa(:,bestIndices), xb(:,bestIndices), Na_bestInd, Nb_bestInd);
+
+        Hxa = pflat(H*xa);
+        dists = sqrt(sum((xb - Hxa).^2, 1));
+
+        Hs{common_ind} = bestH;
+
+        %% Transform A to B using the "useful matlab commands" in assign 4
+
+        tform = maketform('projective',bestH');
+        %Creates a transfomation  that  matlab  can use for  images
+        %Note: imtransform  uses  the  transposed  homography
+        transfbounds = findbounds(tform ,[1 1; size(im{ind},2)  size(im{ind} ,1)]);
+        %Finds  the  bounds  of the  transformed  image
+        xdata = [min([ transfbounds(:,1); 1]) max([ transfbounds(:,1);  size(im{ind_prime} ,2)])];
+        ydata = [min([ transfbounds(:,2); 1]) max([ transfbounds(:,2);  size(im{ind_prime} ,1)])];
+        %Computes  bounds  of a new  image  such  that  both  the old  ones  will  fit.
+        [newA] = imtransform(im{ind},tform ,'xdata',xdata ,'ydata',ydata);
+        %Transform  the  image  using  bestH
+        tform2 = maketform('projective',eye(3));
+        [newB] = imtransform(im{ind_prime},tform2 ,'xdata',xdata ,'ydata',ydata ,'size',size(newA ));
+        %Creates a larger  version  of B
+        newAB = newB;
+        newAB(newB < newA) = newA(newB < newA);
+        %Writes  both  images  in the new  image. %(A somewhat  hacky  solution  is  needed
+        %since  pixels  outside  the  valid  image  area  are not  allways  zero ...)
+
+        figure(common_ind)
+        imagesc(newAB)
+        merged_ims{common_ind} = newAB
     end
 end
+save('ex5_calculations', 'Hs', 'matches', 'merged_ims')
 
-nbr_inliers = sum(bestInliers)
-%bestH = getH(xa(:,bestIndices), xb(:,bestIndices));
-%should be done using normalization when it works
-Na_bestInd = getN(xa(:,bestIndices));
-Nb_bestInd = getN(xb(:,bestIndices));
-bestH = getH(xa(:,bestIndices), xb(:,bestIndices), Na_bestInd, Nb_bestInd);
+%% Show all in one figure 
+%All images has to be included once, meaning each image provides one
+%transf.
+%It was found that all images was looking good when transferred to image2:
+%I will therefore just merge all these images together. 
 
-Hxa = pflat(H*xa);
-dists = sqrt(sum((xb - Hxa).^2, 1));
+h21 = Hs{21};
+h23 = Hs{23};
+h24 = Hs{24};
 
-Hs{common_ind} = bestH;
+%% Transform A1 A2 A3 to B using the "useful matlab commands" in assign 4
 
-%% Transform A to B using the "useful matlab commands" in assign 4
-
-tform = maketform('projective',bestH');
+tform1 = maketform('projective',h21');
+tform3 = maketform('projective',h23');
+tform4 = maketform('projective',h24');
 %Creates a transfomation  that  matlab  can use for  images
 %Note: imtransform  uses  the  transposed  homography
-transfbounds = findbounds(tform ,[1 1; size(im{ind_prime},2)  size(im{ind_prime} ,1)]);
+transfbounds1 = findbounds(tform1 ,[1 1; size(im{1},2)  size(im{1} ,1)]);
+transfbounds3 = findbounds(tform3 ,[1 1; size(im{3},2)  size(im{3} ,1)]);
+transfbounds4 = findbounds(tform4 ,[1 1; size(im{4},2)  size(im{4} ,1)]);
+
+transfbounds = [transfbounds1; transfbounds3; transfbounds4];
 %Finds  the  bounds  of the  transformed  image
-xdata = [min([ transfbounds(:,1); 1]) max([ transfbounds(:,1);  size(im{ind} ,2)])];
-ydata = [min([ transfbounds(:,2); 1]) max([ transfbounds(:,2);  size(im{ind} ,1)])];
+xdata = [min([ transfbounds(:,1); 1]) max([ transfbounds(:,1);  size(im{2} ,2)])];
+ydata = [min([ transfbounds(:,2); 1]) max([ transfbounds(:,2);  size(im{2} ,1)])];
 %Computes  bounds  of a new  image  such  that  both  the old  ones  will  fit.
-[newA] = imtransform(im{ind_prime},tform ,'xdata',xdata ,'ydata',ydata);
+[new1] = imtransform(im{1},tform1 ,'xdata',xdata ,'ydata',ydata);
+[new3] = imtransform(im{3},tform3 ,'xdata',xdata ,'ydata',ydata);
+[new4] = imtransform(im{4},tform4 ,'xdata',xdata ,'ydata',ydata);
 %Transform  the  image  using  bestH
 tform2 = maketform('projective',eye(3));
-[newB] = imtransform(im{ind},tform2 ,'xdata',xdata ,'ydata',ydata ,'size',size(newA ));
+[new2] = imtransform(im{2},tform2 ,'xdata',xdata ,'ydata',ydata ,'size',size(new1));
 %Creates a larger  version  of B
-newAB = newB;
-newAB(newB < newA) = newA(newB < newA);
+new21 = new2;
+new21(new2 < new1) = new1(new2 < new1);
 %Writes  both  images  in the new  image. %(A somewhat  hacky  solution  is  needed
 %since  pixels  outside  the  valid  image  area  are not  allways  zero ...)
 
-figure(common_ind+1)
-imagesc(newAB)
+figure(121)
+imagesc(new21)
+
+new213 = new21;
+new213(new21 < new3) = new3(new21 < new3);
+
+figure(123)
+imagesc(new213);
+
+new2134 = new213;
+new2134(new213 < new4) = new4(new213 < new4);
+
+figure(124)
+imagesc(new2134)
+
+save('ex5_end_image', 'new2134')
+
+%% Further improvements:
+%Maybe find the homography from the complete image new2134 to a paper of
+%size a4. This gives the homography that then can be applied to all other
+%homographies so that they instead transfer to this a4 paper.
